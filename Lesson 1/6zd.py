c81ddb74403ca4f36a00674c2062568cac4a54ab
Revyakin M.O <corergod@gmail.com>
2021-10-08 16:38:20 +0300

    Rename

---
+++ Lesson 1/6zd.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Поменяйте значения переменных x и y местами.
+
+print("Введите x:")
+x = int(input())
+print("Введите y:")
+y = int(input())
+
+x,y = y,x
+
+print("x =",x,"y =",y)
